63164de3d5d1e8da29249451625473af846c7b23
mnhnielsen <mnhnielsen@gmail.com>
2022-03-10 09:27:52 +0100

    Netbeans setup

---
+++ Project/Core/src/main/java/org/example/main/GameInstaller.java
@@ -0,0 +1,12 @@
+package org.example.main;
+import org.openide.modules.ModuleInstall;
+
+
+public class GameInstaller extends ModuleInstall
+{
+    @Override
+    public void restored()
+    {
+        System.out.println("Core modulesssssssssssssssssssssss");
+    }
+}
